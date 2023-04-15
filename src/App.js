5a61cd45df78e2eca7e0321235058a5da7a9c0c0
Vitukaa <vitalija.dau@gmail.com>
2023-04-15 23:55:57 +0300

    page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/main-page' element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <MainPage />
   );
 }
 
